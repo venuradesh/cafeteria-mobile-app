0364e047e7e0db4bcec1513667cfd4c3a4c31bc2
SamithaPrabath <samithaprabath513@gmail.com>
2022-08-08 16:13:33 +0530

    Sign-in-backend

--- Screens/LoginPage.js
+++ Screens/LoginPage.js
@@ -1,11 +1,37 @@
 import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import globalStyles from "../Globals/globalStyles";
+import { collection, getDocs } from "firebase/firestore"; 
+import {db} from '../Firebase/firebase';
 
 const LoginPage = () => {
-  const onSubmitClick = () => {};
 
-  const onResetClick = () => {};
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const checkLogin=async()=>{
+
+    
+    try {
+      const querySnapshot1 = await getDocs(collection(db, "clients"));
+      querySnapshot1.forEach((doc) => {
+        if(doc.data().userName==userName && doc.data().password==password){
+          
+          console.log('Client a'+doc.data().userName+' '+doc.data().password);
+        }
+        
+      });
+      const querySnapshot2 = await getDocs(collection(db, "caterers"));
+      querySnapshot2.forEach((doc) => {
+        if(doc.data().userName==userName && doc.data().password==password){
+          console.log('Caterer'+doc.data().userName+' '+doc.data().password);
+        }
+        
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
 
   return (
     <View style={[globalStyles.container, styles.container]}>
@@ -15,19 +41,19 @@ const LoginPage = () => {
         </View>
         <View style={styles.inputItemsContainer}>
           <View style={styles.inputItem}>
-            <Text style={styles.inputTextContent}>User Name</Text>
-            <TextInput style={styles.input} />
+            <Text style={styles.inputTextContent} >User Name</Text>
+            <TextInput style={styles.input} onChangeText={(val) => setUserName(val)} />
           </View>
           <View style={styles.inputItem}>
-            <Text style={styles.inputTextContent}>Password</Text>
-            <TextInput style={styles.input} />
+            <Text style={styles.inputTextContent} >Password</Text>
+            <TextInput style={styles.input} onChangeText={(val) => setPassword(val)} />
           </View>
         </View>
         <View style={styles.btnContainer}>
-          <Pressable style={[styles.btn, styles.btnSubmit]} onPress={onSubmitClick}>
+          <Pressable style={[styles.btn, styles.btnSubmit]} onPress={checkLogin}>
             <Text style={styles.btnContent}>Submit</Text>
           </Pressable>
-          <Pressable style={[styles.btn, styles.btnReset]} onPress={onResetClick}>
+          <Pressable style={[styles.btn, styles.btnReset]}>
             <Text style={styles.btnContent}>Reset</Text>
           </Pressable>
         </View>
