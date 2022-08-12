8bf5b7999e4b0996c892a449446290066be5cd2e
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2022-08-12 16:52:33 +0530

    logout section started

---
+++ Screens/Logout.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const LogOut = () => {
+  return (
+    <View>
+      <Text>LogOut</Text>
+    </View>
+  );
+};
+
+export default LogOut;
+
+const styles = StyleSheet.create({});
